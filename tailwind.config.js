0ae8e8fe4e411cc9e701d4acd66fedf00b3048c6
Marienoir <okosunmaryeghonghon@gmail.com>
2023-09-16 00:09:59 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        black: "#101010",
+      },
+      // colors: {
+      //   custom: "#8D8BA1",
+      // },
+    },
+    fontFamily: {
+      satoshi: ["Satoshi", "sans"],
+    },
+  },
+  plugins: [],
+};
