3d31d806bb560d028db6d29a1eb2e3febde6609d
Marienoir <okosunmaryeghonghon@gmail.com>
2023-09-16 02:49:45 +0100

    include responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // purge: [
+  //   './src/*.html',
+  //   './src/**/*.js',
+  // ],
   content: [],
   theme: {
+    fontSize: {
+      title: `2.6rem;`,
+      paragraph: `1.2rem;`,
+    },
     extend: {
       backgroundColor: {
         black: "#101010",
@@ -10,9 +18,12 @@ module.exports = {
       //   custom: "#8D8BA1",
       // },
     },
-    fontFamily: {
-      satoshi: ["Satoshi", "sans"],
-    },
+    // fontFamily: {
+    //   satoshi: ["Satoshi", "sans"],
+    // },
+    // fontFamily: {
+    //   poppins: ["Poppins"],
+    // },
   },
   plugins: [],
 };
