6db4f0b8795f161c70f9043a4682be5448acdaf4
Marienoir <okosunmaryeghonghon@gmail.com>
2023-09-16 23:04:13 +0100

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // purge: [
-  //   './src/*.html',
-  //   './src/**/*.js',
-  // ],
   content: [],
   theme: {
     fontSize: {
       title: `2.6rem;`,
       paragraph: `1.2rem;`,
     },
-
     extend: {
-      // backgroundColor: {
-      //   black: "#00ADEF",
-      //   // blue: "#00ADEF",
-      // },
       colors: {
         white: "#ffffff",
         secondary: "#8D8BA1",
